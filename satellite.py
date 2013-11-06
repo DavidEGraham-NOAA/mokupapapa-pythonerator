2f430c8c11b525d03facfd2573efc0966d0eb50a
David Graham <david.graham@noaa.gov>
2013-11-06 09:48:19 -1000

    Create satellite.py

---
+++ satellite.py
@@ -0,0 +1,25 @@
+import hashlib
+import os
+import urllib2
+import mailer
+
+req = urllib2.Request("http://www.prh.noaa.gov/hnl/graphics/gtwo_gsat.gif")
+response = urllib2.urlopen(req)
+BASE_DIR = '/home/dgraham/Documents/'
+output = open(BASE_DIR + 'new_file.gif','wb')
+output.write(response.read())
+output.close()
+#IOError:
+newmd5 = hashlib.md5(open(BASE_DIR + 'new_file.gif').read()).hexdigest()
+#print(str(newmd5))
+
+oldmd5 = hashlib.md5(open(BASE_DIR + 'gtwo_gsat.gif').read()).hexdigest()
+#print(str(oldmd5))
+
+if(newmd5 != oldmd5):
+    os.remove(BASE_DIR + 'gtwo_gsat.gif')
+    os.rename(BASE_DIR + 'new_file.gif', BASE_DIR + 'gtwo_gsat.gif')
+    mailer.NotificationMail('david.graham@noaa.gov;gimmesomebeach@gmail.com', 'New Satellite Image', 'Got a new satellite image')
+else:
+    os.remove(BASE_DIR + 'new_file.gif')
+    #mailer.NotificationMail('david.graham@noaa.gov', 'Old Satellite Image', 'Got an old satellite image')
