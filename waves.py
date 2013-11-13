a08ee1c26e8ac034cde727401316f39513932fb7
David Graham <david.graham@noaa.gov>
2013-11-13 11:09:19 -1000

    Correct exception handling.
    Modified exception handling/mailing. Corrected exception message parsing for sending to mailer.

--- waves.py
+++ waves.py
@@ -2,30 +2,38 @@ import datetime
 import re
 import urllib2
 import weatherdb
+import mailer
 
 '''David Graham 10/28/2013'''
 '''Retrieves the wave height and direction in Hilo Bay'''
+notificationlist = [<EMAIL_ADDRESSES>]
+try:
+    splitter=[]
+    #get current datetime in UTC (HST +10 hours)
+    dt = datetime.datetime.now() + datetime.timedelta(hours=10)
+    datest = dt.strftime("%Y%m%d")
 
-splitter=[]
-#get current datetime in UTC (+10 hours)
-dt = datetime.datetime.now() + datetime.timedelta(hours=10)
-datest = dt.strftime("%Y%m%d")
+    url = 'http://cdip.ucsd.edu/cgi-bin/pm_download?station=188&year='+dt.strftime("%Y")+'&month='+dt.strftime("%m")+'&public=public&stream_label=p1'
 
-url = 'http://cdip.ucsd.edu/cgi-bin/pm_download?station=188&year='+dt.strftime("%Y")+'&month='+dt.strftime("%m")+'&public=public&stream_label=p1'
+    req = urllib2.Request(url)
+    response = urllib2.urlopen(req)
+    the_page = response.read()
 
-req = urllib2.Request(url)
-response = urllib2.urlopen(req)
-the_page = response.read()
+    #split the url response into lines which gives us an array from which we then remove excess whitespace
+    lines = the_page.split('\n')
+    lines = [line.strip() for line in lines]
 
-#split the url response into lines which gives us an array from which we then remove excess whitespace
-lines = the_page.split('\n')
-lines = [line.strip() for line in lines]
+    #the first four lines are headers and the last line is empty
+    for line in lines[4:-1]:
+        splitter = re.split('\s+', line)
+        obsdate = datetime.datetime(int(splitter[0]), int(splitter[1]), int(splitter[2]), int(splitter[3]), int(splitter[4]))
+        wave_height = splitter[5]
+        wave_direction = splitter[7]
+        wave_period = splitter[6]
+        weatherdb.SaveWaves(obsdate, wave_height, wave_direction, wave_period)
 
-#the first four lines are headers and the last line is empty
-for line in lines[4:-1]:
-    splitter = re.split('\s+', line)
-    obsdate = datetime.datetime(int(splitter[0]), int(splitter[1]), int(splitter[2]), int(splitter[3]), int(splitter[4]))
-    wave_height = splitter[5]
-    wave_direction = splitter[7]
-    wave_period = splitter[6]
-    weatherdb.SaveWaves(obsdate, wave_height, wave_direction, wave_period)
+    #mailer.NotificationMail(notificationlist, 'Weather Portal - waves', 'Waves worked.')
+
+except Exception as e:
+    msg = 'Processing error saving wave observations. See waves.py and mailx. URL requested was = ' + url
+    mailer.NotificationMail(notificationlist, 'Weather Portal Error - waves', msg + str(repr(e)))
