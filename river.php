f28443d2b945dfad3dabfde87dfac467f120f233
David Graham <david.graham@noaa.gov>
2013-12-02 16:29:09 -0500

    Create river.php

---
+++ river.php
@@ -0,0 +1,18 @@
+<?php
+require '../includes/mpp_dbconn.php';
+header('Cache-Control: no-cache, must-revalidate');
+header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
+header('Content-type: application/json');
+
+$raindate = pg_fetch_result($res, 0, 0);
+$result = pg_query($db, "SELECT TO_CHAR(observationdate,'YYYYMMDDHH24MI') AS observationdate, flowrate, height FROM river WHERE observationdate >= current_date - interval '30 days' ORDER BY observationdate");
+if (!$result) {
+	die("Error in SQL query: " . pg_last_error());
+}
+$results = array(array());
+while ($row = pg_fetch_array($result)) {
+	echo $row['observationdate'] . "," . $row['flowrate'] . "," . $row['height'] . "\n"; 
+	$results[] = $row;
+}
+pg_free_result($result);
+?>
