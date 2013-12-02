8869b81596ae3da9e6d0a01fa2348696845b4fb4
David Graham <david.graham@noaa.gov>
2013-12-02 16:28:22 -0500

    Update rain.php

--- rain.php
+++ rain.php
@@ -1,9 +1,18 @@
 <?php
 require '../includes/mpp_dbconn.php';
+header('Cache-Control: no-cache, must-revalidate');
+header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
+header('Content-type: application/json');
 
-$res = pg_query($db, "SELECT max(createdate) as thedate FROM rain;");
-if(!$res){
+$raindate = pg_fetch_result($res, 0, 0);
+$result = pg_query($db, "SELECT TO_CHAR(observationdate,'YYYYMMDDHH24MI') AS observationdate, precipitation FROM rain WHERE observationdate >= current_date - interval '30 days' ORDER BY observationdate");
+if (!$result) {
 	die("Error in SQL query: " . pg_last_error());
 }
-$raindate = pg_fetch_result($res, 0, 0);
+$results = array(array());
+while ($row = pg_fetch_array($result)) {
+	echo $row['observationdate'] . "," . $row['precipitation'] . "\n"; 
+	$results[] = $row;
+}
+pg_free_result($result);
 ?>
