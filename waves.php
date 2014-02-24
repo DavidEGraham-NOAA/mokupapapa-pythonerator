d47dd7e0e054e985ebcddde62e202f8059ebcc81
David Graham <david.graham@noaa.gov>
2014-02-24 11:42:52 -1000

    Change retrieval interval to 75 days
    Changed retrieval interval to 75 days, added comments.

--- waves.php
+++ waves.php
@@ -1,18 +1,35 @@
 <?php
+//David Graham
+//queries and returns wave observations for the last 75 days from mokupapapa.waves table
+//last row returned is the mean height and period observations from last 365 days
 require '../includes/mpp_dbconn.php';
 header('Cache-Control: no-cache, must-revalidate');
 header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
 header('Content-type: application/json');
 
-$raindate = pg_fetch_result($res, 0, 0);
-$result = pg_query($db, "SELECT TO_CHAR(observationdate,'YYYYMMDDHH24MI') AS observationdate, height, direction, period FROM waves WHERE observationdate >= current_date - interval '30 days' ORDER BY observationdate");
+$result = pg_query($db, "SELECT TO_CHAR(observationdate,'YYYYMMDDHH24MI') AS observationdate, height, direction, period 
+FROM waves 
+WHERE observationdate >= current_date - interval '75 days' 
+AND height IS NOT NULL 
+ORDER BY observationdate");
 if (!$result) {
-	die("Error in SQL query: " . pg_last_error());
+        die("Error in SQL query: " . pg_last_error());
+} else {
+    echo "date,height,direction,period";
+    while ($row = pg_fetch_array($result)) {
+        echo "\n" . $row['observationdate'] . "," . $row['height'] . "," . $row['direction'] . "," . $row['period'];
+    }
+    pg_free_result($result);
+
+    $avgresult = pg_query($db, "SELECT TO_CHAR(now(),'YYYYMMDDHH24MI') AS observationdate, avg(height), avg(period) 
+FROM waves 
+WHERE observationdate >= current_date - interval '365 days'");
+    if(!$avgresult){
+        //do nothing
+    } else {
+        $row = pg_fetch_row($avgresult);
+        echo "\n" . $row[0] . "," . $row[1] . ",," . $row[2];
+    }
+    pg_free_result($avgresult);
 }
-$results = array(array());
-while ($row = pg_fetch_array($result)) {
-	echo $row['observationdate'] . "," . $row['height'] . "," . $row['direction'] . "," . $row['period'] . "\n";
-	$results[] = $row;
-}
-pg_free_result($result);
 ?>
