c857b17c1672057d53e996c5ef541c637183a0da
David Graham <david.graham@noaa.gov>
2014-02-24 11:46:57 -1000

    Change retrieval interval to 75 days
    Changed retrieval interval to 75 days, added comments.

--- wind.php
+++ wind.php
@@ -1,18 +1,37 @@
 <?php
+//David Graham
+//queries and returns wind observations for the last 75 days from mokupapapa.wind table
+//last row returned is the mean speed observations from last 365 days
 require '../includes/mpp_dbconn.php';
 header('Cache-Control: no-cache, must-revalidate');
 header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
 header('Content-type: application/json');
 
-$raindate = pg_fetch_result($res, 0, 0);
-$result = pg_query($db, "SELECT TO_CHAR(observationdate,'YYYYMMDDHH24MI') AS observationdate, speed, direction FROM wind WHERE observationdate >= current_date - interval '30 days' ORDER BY observationdate");
+$result = pg_query($db, "SELECT TO_CHAR(observationdate,'YYYYMMDDHH24MI') AS observationdate, speed, direction 
+FROM wind 
+WHERE observationdate >= current_date - interval '75 days' 
+AND observationdate IS NOT NULL 
+ORDER BY observationdate");
 if (!$result) {
-	die("Error in SQL query: " . pg_last_error());
+        die("Error in SQL query: " . pg_last_error());
+} else {
+    echo "date,speed,direction";
+    $results = array(array());
+    while ($row = pg_fetch_array($result)) {
+        echo "\n" . $row['observationdate'] . "," . $row['speed'] . "," . $row['direction'];
+    }
+    pg_free_result($result);
+
+    $avgresult = pg_query($db, "SELECT TO_CHAR(now(),'YYYYMMDDHH24MI') AS observationdate, avg(speed) 
+FROM wind 
+WHERE observationdate >= current_date - interval '365 days' 
+ORDER BY observationdate");
+    if(!$avgresult){
+        //do nothing
+    } else {
+        $row = pg_fetch_row($avgresult);
+        echo "\n" . $row[0] . "," . $row[1] . ",";
+    }
+    pg_free_result($avgresult);
 }
-$results = array(array());
-while ($row = pg_fetch_array($result)) {
-	echo $row['observationdate'] . "," . $row['speed'] . "," . $row['direction'] . "\n";
-	$results[] = $row;
-}
-pg_free_result($result);
 ?>
